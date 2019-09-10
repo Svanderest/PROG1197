c080c64db85fc09ac4a32e18930bd75bc9dcf7b6
Sebastian Van Der Est <svanderest1@ncstudents.niagaracollege.ca>
2019-09-10 11:03:24 -0400

    initial code commit

---
+++ Sep10Console/PROG1197/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PROG1197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get text form user
+            string myString;
+            do
+            {
+                Console.WriteLine("Please enter a word or type \"Exit\" to quit");
+                myString = Console.ReadLine();
+            } while (myString.Length == 0);
+
+            //Check for exit
+            if(myString.ToLower() != "exit")
+            {
+                //Get number from user
+                int myIndex = -1;
+                do
+                {
+                    Console.WriteLine(string.Format("Please enter a position number in the text: {0}",myString));
+                } while (!int.TryParse(Console.ReadLine(), out myIndex) || myIndex >= myString.Length);
+
+                //Return result
+                Console.WriteLine(string.Format("The character at position {0} is {1}", new object[] { myIndex, myString[myIndex] }));
+            }            
+        }
+    }
+}
