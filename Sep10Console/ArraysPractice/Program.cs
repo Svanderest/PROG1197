00d4af2ec1249e1b6486e985d5b3b8a30d37e496
Sebastian Van Der Est <svanderest1@ncstudents.niagaracollege.ca>
2019-09-11 13:21:06 -0400

    added arrays practice

---
+++ Sep10Console/ArraysPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArraysPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grades = new int[5][];
+            grades[0] = new int[2];
+            grades[1] = new int[4];
+            grades[2] = new int[1];
+            grades[3] = new int[3];
+            grades[4] = new int[5];
+
+            for(int i = 0; i < grades.Length; i++)
+            {
+                for(int j = 0; j < grades[i].Length; j++)
+                {
+                    do
+                    {
+                        Console.WriteLine(string.Format("Please enter your grade for test {0} in Course {1}", new object[] { j, i }));
+                    } while (!int.TryParse(Console.ReadLine(), out grades[i][j]) || grades[i][j] < 0 || grades[i][j] > 100);
+                }
+            }
+        }
+    }
+}
